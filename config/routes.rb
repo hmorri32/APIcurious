ebc151d917dbe1fcda8e688507bf432864fd0fab
Hugh Morrison <hmorri32@gmail.com>
2017-12-05 16:33:49 -0700

    get auth to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
 
-  get '/auth/github',          as: :login_path
+  get '/auth/github',          as: :login
   get '/auth/github/callback', to: 'sessions#create'
   get 'dashboard',             to: 'users#show'
   delete '/logout',            to: 'sessions#destroy'
