14463bda63ad2b87d1e8b652e0248a8cebb1d35b
Hugh Morrison <hmorri32@gmail.com>
2017-12-05 15:30:52 -0700

    g user model and run migration

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    username "MyString"
+    avatar_url "MyString"
+    email "MyString"
+    uid "MyString"
+    provider "MyString"
+    oauth_token "MyString"
+  end
+end
