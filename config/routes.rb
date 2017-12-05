3337cd35dfe85c9c126c5e44c2cce6a0d53bf08d
Hugh Morrison <hmorri32@gmail.com>
2017-12-05 16:18:13 -0700

    adds dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root "home#index"
 
-  get '/auth/github',          as: :github_login
+  get '/auth/github',          as: :login_path
   get '/auth/github/callback', to: 'sessions#create'
+  get 'dashboard',             to: 'users#show'
   delete '/logout',            to: 'sessions#destroy'
 end
