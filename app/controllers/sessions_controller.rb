71bba1695b4c4aae3101f3ec39476cae730dad31
Hugh Morrison <hmorri32@gmail.com>
2017-12-06 18:39:31 -0700

    redirects to user show if success

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.valid?
       session[:user_id] = user.id
+      redirect_to dashboard_path
+    else
       redirect_to request.env['omniauth.origin']
     end
   end
