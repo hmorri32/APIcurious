ba1b467e888b64ae51a96729cc61aa7c486ee6d9
Hugh Morrison <hmorri32@gmail.com>
2017-12-06 20:39:32 -0700

    reactive

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -1,13 +1,42 @@
 // Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
+// webpack-dev-server
 
-import React      from 'react'
+import React, { Component }      from 'react'
 import { render } from 'react-dom'
 import PropTypes  from 'prop-types'
 
-const Hello = props => <div>Hello { props.name }!</div>
+class Hello extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'cool guy jones',
+      date: ['array', 'of', 'stuff'],
+    }
+  }
+  componentWillMount() {
+    this.setState({ coolStuff: [1,2,3,4] })
+  }
+
+  render() {
+    return (
+      <div>
+        <p> { this.state.name } </p>
+        <p> { this.state.date.map(thing => `${thing} `) } </p>
+        <p> { this.state.coolStuff.map(thing => `${thing} `) } </p>
+      </div>
+    )
+  }
+}
+
+// const Hello = props => {
+//   console.log()
+//   return (
+//   <div>Hello { props.name }!</div>
+// )}
+
 
 document.addEventListener('DOMContentLoaded', () => {
-  render(<Hello name="coolguy mcgee" />, document.getElementById('put_react_here'))
+  render(<Hello />, document.getElementById('put_react_here'))
 })
