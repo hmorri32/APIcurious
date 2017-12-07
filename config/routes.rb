8e1698c27ce9db5078b3a965e764dbaa2ccbe0b3
Hugh Morrison <hmorri32@gmail.com>
2017-12-07 14:05:38 -0700

    adds namespaced api routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
   get '/auth/github/callback', to: 'sessions#create'
   get 'dashboard',             to: 'users#show'
   delete '/logout',            to: 'sessions#destroy'
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      get 'follower_count', to: "git_hub#follower_count"
+      get 'user',           to: "git_hub#user"
+    end
+  end
 end
