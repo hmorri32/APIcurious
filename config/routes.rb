79e5cc6b6cbe301484e9ed7744c559719fe5e9b6
Hugh Morrison <hmorri32@gmail.com>
2017-12-07 20:20:22 -0700

    defines namespace for github api

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,14 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      get 'followers', to: "git_hub#followers"
-      get 'user',      to: "git_hub#user"
+      get '/user',               to: "git_hub#user"
+      get '/followers',          to: "git_hub#followers"
+      get '/following',          to: "git_hub#following"
+      get '/starred_repos',      to: "git_hub#starred_repos"
+      get '/user_repos',         to: "git_hub#user_repos"
+      get '/user_organizations', to: "git_hub#user_organizations"
+      get '/received_events',    to: "git_hub#received_events"
+      get '/user_events',        to: "git_hub#user_events"
     end
   end
 end
