feb99a6b14667b58c83ca9b3e2dd253368605143
Hugh Morrison <hmorri32@gmail.com>
2017-12-07 20:19:39 -0700

    builds out basic functionality using JSON

--- app/controllers/api/v1/git_hub_controller.rb
+++ app/controllers/api/v1/git_hub_controller.rb
@@ -1,9 +1,37 @@
 class Api::V1::GitHubController < ApplicationController
   def user
-    render json: { username: current_user.username, avatar_url: current_user.avatar_url }
+    json_response({
+      username:   current_user.username,
+      avatar_url: current_user.avatar_url
+    })
   end
 
   def followers
-    render json: GitHubService.new(current_user).followers
+    json_response(current_user.followers)
+  end
+
+  def following
+    json_response(current_user.following)
+  end
+
+  def starred_repos
+    json_response(current_user.starred_repos)
+  end
+
+  def user_repos
+    json_response(current_user.user_repos)
+  end
+
+  def user_organizations
+    json_response(current_user.user_organizations)
+  end
+
+  def received_events
+    render json: current_user.received_events
+  end
+
+  def user_events
+    # render json: GitHubService.new(current_user).user_events
+    render json: current_user.user_events
   end
 end
